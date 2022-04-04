e45f87ab0b915cc3aa569e72dfe31094fc10cbd4
William <willyknapp@gmail.com>
2022-04-04 11:01:17 -0400

    leaflet-pip integration

--- webmap.js
+++ webmap.js
@@ -1,5 +1,6 @@
 // Import the leaflet package
 var L = require('leaflet');
+var leafletPip = require('@mapbox/leaflet-pip');
 
 // Creates a leaflet map binded to an html <div> with id "map"
 // setView will set the initial map view to the location at coordinates
@@ -14,7 +15,7 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
 	maxZoom: 21
 }).addTo(map);
 
-const eurasiaCoords = ([
+const continentCoords = [[
   [72,180],
   [76.5,142],
   [78,104],
@@ -64,9 +65,8 @@ const eurasiaCoords = ([
   [49,150],
   [50,160],
   [63,180]
-]);
-
-const americasCoords = [
+],
+[
   [63,-180],
   [56.5,-162],
   [53.5,-169.5],
@@ -108,10 +108,9 @@ const americasCoords = [
   [70,-137],
   [72,-157],
   [72,-180]
-]
+]];
 
-var eurasiaPoly = L.polygon(eurasiaCoords, {color:'red'}).addTo(map);
-var americasPoly = L.polygon(americasCoords, {color:'red'}).addTo(map);
+var continentPoly = L.polygon(continentCoords, {color:'red'}).addTo(map);
 
 var primeMeridian = L.polyline([
   [-90,0],
@@ -130,3 +129,25 @@ var border = L.polyline([
   [-90,180],
   [-90,-180]
 ], {color:'white'}).addTo(map);
+
+var popup = L.popup();
+var data = continentPoly.toGeoJSON()
+var gjLayer = L.geoJson(data);
+
+function onClick(evt) {
+	console.log('Clicked');
+	var results = leafletPip.pointInLayer(evt.latlng, gjLayer);
+	console.log('results: ' + results);
+	var message;
+	if(results && results.length) {
+		message = "Point is in continent polygon.";
+	} else {
+		message = "Point is not in continent polygon.";
+	}
+	console.log(message);
+		popup
+		.setLatLng(evt.latlng)
+    .setContent(message + " (Coords: " + evt.latlng.toString() + ")")
+    .openOn(map);
+}
+map.on('click', onClick);
